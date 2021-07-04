5f020f556b44de201a65d67d3c4ed504f9ad7659
Trung Doan <doanpt93cntt@gmail.com>
2021-07-04 09:17:55 +0700

    3. popular junit method naming

--- src/test/kotlin/MathUtilTest.kt
+++ src/test/kotlin/MathUtilTest.kt
@@ -2,13 +2,12 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class MathUtilTest {
-    @Test
-    fun test_add() {
-        println("This is demo of test")
-    }
 
+    //We have 7 popular method naming ways, I prefer solution 3 or 4.
+    // For more detail, refer 7-popular-method-naming-in-junit.md file in resource folder
+    // below is testes named follow solution 3.
     @Test
-    fun test_demo1() {
+    fun testAddIfNumberValid() {
         val mathUtil = MathUtils()
         val result = mathUtil.add(5, 5)
         assertEquals(10, result)
