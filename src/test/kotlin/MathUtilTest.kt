689f78341775a21105f8f85603db91c2ad4f1d0b
Trung Doan <doanpt93cntt@gmail.com>
2021-07-04 09:32:52 +0700

    use Display name or backticks to highli improves readability

--- src/test/kotlin/MathUtilTest.kt
+++ src/test/kotlin/MathUtilTest.kt
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -7,7 +8,15 @@ class MathUtilTest {
     // For more detail, refer 7-popular-method-naming-in-junit.md file in resource folder
     // below is testes named follow solution 3.
     @Test
+    @DisplayName("test add if number is valid")
     fun testAddIfNumberValid() {
+        val mathUtil = MathUtils()
+        val result = mathUtil.add(5, 5)
+        assertEquals(10, result)
+    }
+
+    @Test
+    fun `test add if number is valid`() {
         val mathUtil = MathUtils()
         val result = mathUtil.add(5, 5)
         assertEquals(10, result)
