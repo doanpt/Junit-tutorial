454006dab8b7dde5586499eda6f7a9edb5bb1480
Trung Doan <doanpt93cntt@gmail.com>
2021-07-04 22:24:41 +0700

    22. Use Mockito Extension to inject parameter of test method

---
+++ src/test/kotlin/mockito/LibraryManagementMockitoExtensionMethodParameterTest.kt
@@ -0,0 +1,24 @@
+package mockito
+
+import mock.BookService
+import mock.LendBookManager
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class LibraryManagementMockitoExtensionMethodParameterTest {
+
+    @Test
+    fun whenBookIsNotAvailable_thenAnExceptionIsThrown(@Mock bookService: BookService) {
+        val manager = LendBookManager(bookService)
+        Mockito.`when`(bookService.inStock(100)).thenReturn(false)
+        assertThrows<IllegalStateException> {
+            manager.checkout(100, 1)
+        }
+    }
+
+}
